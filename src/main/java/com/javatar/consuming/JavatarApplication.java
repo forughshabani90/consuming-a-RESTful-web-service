4c30ec3384a2195b268123f2a24540c354d521e0
forughshabani90 <forugh.shabani.90@gmail.com>
2024-01-27 07:57:57 -0800

    fc

---
+++ src/main/java/com/javatar/consuming/JavatarApplication.java
@@ -0,0 +1,13 @@
+package com.javatar.consuming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavatarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatar/consuming/JavatarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatar.consuming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavatarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
